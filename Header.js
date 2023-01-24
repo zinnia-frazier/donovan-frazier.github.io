4281d7a3dc3d30704b2db25de83513f2299a04b6
donovan-frazier <don7229fraz@gmail.com>
2023-01-24 15:21:31 -0800

    Collapsible header attempt

--- Header.js
+++ Header.js
@@ -10,3 +10,18 @@ if (window.pageYOffset > sticky) {
     header.classList.remove("Sticky");
 }
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
