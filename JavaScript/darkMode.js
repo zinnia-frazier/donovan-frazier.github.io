0c9281e315d0e70552be668c1b6b630c6915c036
donovan-frazier <don7229fraz@gmail.com>
2023-02-22 19:18:41 -0800

    Darkmode addition

---
+++ JavaScript/darkMode.js
@@ -0,0 +1,9 @@
+function toggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+function toggleButtonStyle() {
+    var element = document.getElementById("modeButton");
+    element.classList.toggle("modeButtonStyle");
+}
+
