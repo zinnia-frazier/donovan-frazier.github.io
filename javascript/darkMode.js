b450b8d054ac3ec0be043051fc853fa902c62516
Dawn <120154112+donovan-frazier@users.noreply.github.com>
2023-05-09 15:49:11 -0700

    Add files via upload

---
+++ javascript/darkMode.js
@@ -0,0 +1,12 @@
+function toggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+function toggleButtonStyle() {
+    var element = document.getElementById("modeButton");
+    element.classList.toggle("modeButtonStyle");
+}
+function changeListColor() {
+    var element = document.body;
+    element.classList.toggle('listColor');
+}
