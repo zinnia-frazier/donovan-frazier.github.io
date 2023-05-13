fe05beed9a2931183cb33bd2c58b11fe0f7dc807
Dawn <120154112+donovan-frazier@users.noreply.github.com>
2023-05-13 00:26:15 -0700

    Final revisions

--- javascript/darkMode.js
+++ javascript/darkMode.js
@@ -1,8 +1,16 @@
 function toggleDarkMode() {
+    function checkDarkMode() {
+        if (sessionStorage.getItem('darkMode', false)) {
+        sessionStorage.setItem('darkMode', true)
+        } else {
+            sessionStorage.setItem('darkMode', false)
+        }
+    };
+    
     var element = document.body;
     element.classList.toggle("dark-mode");
-    sessionStorage.setItem('darkMode', true);
 }
+
 function toggleButtonStyle() {
     var element = document.getElementById("modeButton");
     element.classList.toggle("modeButtonStyle");
