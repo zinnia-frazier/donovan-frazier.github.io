e22a24526b75caf849635e21f8a5f55662107b19
Dawn <120154112+donovan-frazier@users.noreply.github.com>
2023-05-19 20:37:16 -0700

    Replace Izzy's portfolio with Dawn LLC

--- javascript/activeButton.js
+++
@@ -1,8 +0,0 @@
-function activeButton() {
-    var element = document.getElementByID("modeButton");
-    element.classList.toggle("active");
-}
-
-document.querySelector('.active').addEventListener('hover', () => {
-    element.classList.remove('active')
-})
